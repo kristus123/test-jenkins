509c86c2de39c2e704e43932cf623d2e52b34f60
kristian.solbakken <kristian.solbakken@dnb.no>
2020-05-04 08:17:17 +0200

    c

--- app/test_main.py
+++ app/test_main.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() != {"message": "Hello World"}
 
 def test_shit():
 	assert 1 + 1 == 2
